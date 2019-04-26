8125840064000d35e6ad6b4cdcfc9ab6a98d781b
chavez102 <bryanchavez102@gmail.com>
2019-04-26 11:08:51 -0400

    message

--- src/bryanTest/Test.java
+++ src/bryanTest/Test.java
@@ -5,6 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		System.out.println("testing command line push");
+		
 	}
 
 }
