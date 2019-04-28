a78f194c711e47e9b481902ffd3895d1975be3d5
chavez102 <bryanchavez102@gmail.com>
2019-04-27 21:47:17 -0400

    Second Commit Testing

--- src/bryanTest/Test.java
+++ src/bryanTest/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
-		System.out.println("testing command line push");
+		System.out.print("using command line push Second time ");
 		
 	}
 
